4d9b4cc26dfa8d78cabffe205ae9ca739fc9b4da
Aiden Williams <aidenwilliams336@gmail.com>
2024-10-07 11:48:10 +0200

    toggle section

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ document.querySelectorAll('.product button').forEach(button => {
         alert('Product added to cart!');
     });
 });
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    const main = document.getElementById("main");
+    
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+        main.style.marginLeft = "0";
+    } else {
+        sidebar.style.width = "250px";
+        main.style.marginLeft = "250px";
+    }
+}
